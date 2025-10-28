fe8805b7a2e859159e6cb7f3db247fbb46432075
ArifDev-coder <achadzainul67@gmail.com>
2025-10-28 16:50:33 +0700

    speed insights vercel

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import RouterWrapper from "./route/RouterWrapper.jsx";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 import "./index.css";
 
@@ -9,5 +10,6 @@ import "remixicon/fonts/remixicon.css";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <RouterWrapper />
+    <SpeedInsights />
   </StrictMode>
 );
